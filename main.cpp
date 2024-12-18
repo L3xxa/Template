232f6bef95c4bc4c71229c2e5c8153fec23809a6
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-12-18 13:17:03 +0200

    not ready

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <ctime>
 using namespace std;
 
+// Створеня масиву
+template <typename T1, typename T2>
+void createArray (T1 *arr, T2 size) {
+    for (int i = 0; i < size; i++) {
+        cout << rand () % 100 << " ";
+    }
+    cout << endl;
+}
+
+// Вивід масиву
+template <typename T1, typename T2>
+void print(T1 *arr, T2 size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+
 int main() {
+
+    srand(time(0));
+    int *arr = new int [10];
+    createArray (arr, 10); // // Створеня масиву
+    print (arr, 10); // Вивід масиву
 
 
     return 0;
